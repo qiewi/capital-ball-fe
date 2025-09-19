6775d8d993c9a14435324c618b83a61d4a97a6e4
Rizqi Andhika <qiewifruit@gmail.com>
2025-09-19 21:08:00 +0700

    feat: add navbar

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "@/components/layout/HeroSection";
+import { Navbar } from "@/components/layout/Navbar";
 import { OutlookSection } from "@/components/Portfolio/OutlookSection";
 
 export default function PortfolioPage() {
   return (
-    <main>
+    <main className="min-h-screen">
+      <Navbar/>
       <HeroSection />
       <OutlookSection />
     </main>
