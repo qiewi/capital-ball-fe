9ead997cec6932b24c054309a0df2db59438fbf9
Rizqi Andhika <qiewifruit@gmail.com>
2025-09-19 21:00:37 +0700

    feat: portfolio page

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+import { HeroSection } from "@/components/layout/HeroSection";
+import { OutlookSection } from "@/components/Portfolio/OutlookSection";
+
+export default function PortfolioPage() {
+  return (
+    <main>
+      <HeroSection />
+      <OutlookSection />
+    </main>
+  );
+}
